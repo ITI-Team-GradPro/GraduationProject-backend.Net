13c31ab9ac03dc9aec60458dbc172b5222f5daef
Tasneem <Tasneem@TASNEEM>
2024-03-05 14:35:30 +0200

    Adding New Model "Black List" and their properties

---
+++ GraduationProject/Data/Models/BlackList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraduationProject.Data.Models 
+{
+    public class BlackList
+{
+        public String Email { get; set; }
+        public int AdminId { get; set; }
+        public string BanReason { get; set; }
+        public DateTime BanDate { get; set; }
+
+    }
+}
