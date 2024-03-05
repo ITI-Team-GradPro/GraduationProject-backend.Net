688243dd0bd830193ee8ee4b3007c6a081c1e6a6
MustafaMahmoud1 <106413978+MustafaMahmoud1@users.noreply.github.com>
2024-03-05 14:42:12 +0200

    Booking Model

--- GraduationProject/Data/Context/GP-Db.cs
+++ GraduationProject/Data/Context/GP-Db.cs
@@ -7,9 +7,6 @@ namespace GraduationProject.Data.Context
     internal class GP_Db : DbContext
     {
 
-
-        public DbSet<User> Users { get; set; }
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=GP-Evently;Integrated Security=True;Trust Server Certificate=True");
