c4b5be222c1f2f249b45fc6caeee8370f7848f76
Safi-Abu-Saad <alsafyabosaad3@gmail.com>
2024-03-05 14:40:04 +0200

    review/comment

--- GraduationProject/Data/Models/Comments.cs
+++ GraduationProject/Data/Models/Comments.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +9,18 @@ using System.Threading.Tasks;
 namespace GraduationProject.Data.Models
 {
     internal class Comments
-{
-}
+    {
+        [Key]
+        public int CommentID { get; set; }
+
+        [ForeignKey]
+        public int UserID { get; set; }
+
+        [ForeignKey]
+        public int PlaceID { get; set; }
+
+        public string CommentText { get; set; }
+
+        public DateTime CommentDate { get; set; }
+    }
 }
--- GraduationProject/Data/Models/Reviews.cs
+++ GraduationProject/Data/Models/Reviews.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Runtime;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GraduationProject.Data.Models
 {
     internal class Reviews
-{
-}
+    {
+        [Key]
+        public int ReviewID { get; set; }
+
+        [ForeignKey]
+        public int UserID { get; set; }
+
+        [ForeignKey]
+        public int PlaceID { get; set; }
+
+        public DateTime ReviewDate { get; set; }
+
+        public string ReviewText { get; set; }
+
+        public double Rating { get; set; }
+    }
 }
