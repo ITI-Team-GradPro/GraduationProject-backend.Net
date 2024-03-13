4f4b80fc75849300e00d3fad320354f3a8187bfb
amany-adel9955 <155364829+amany-adel9955@users.noreply.github.com>
2024-03-13 17:48:08 +0200

    edit Dto

---
+++ GraduationProject/Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using GraduationProject.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraduationProject.DAL;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly GP_Db _context;
+    public UnitOfWork(GP_Db context, IUserRepo userRepo)
+    {
+        _context = context;
+        UserRepo = userRepo;
+         
+    }
+    public IUserRepo UserRepo { get; }
+ 
+
+    public int SaveChanges()
+    {
+        return _context.SaveChanges();
+    }
+}
