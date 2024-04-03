74bae410fe8fa4d6fde15cfe881ca32767d2f99f
Tasneem <Tasneem@TASNEEM>
2024-04-03 14:32:22 +0200

    Adding End point to Change Password

---
+++ GraduationProject.DL/Dtos/UserDto/ChangePasswordDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraduationProject.BL.Dtos.UserDto
+{
+    public class ChangePasswordDto
+    {
+        [Required]
+        [MinLength(8)]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [MinLength(8)]
+        public string NewPassword { get; set; }
+
+        public string Email { get; set; }
+    }
+}
